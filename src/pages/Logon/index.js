65553e200fe6cb01a53c4e1f549bdf84495e73ee
Rafael Rabelo <rafael_rabelo@live.com>
2020-10-24 20:16:18 -0300

    Migra pagina template de login

--- src/pages/Logon/index.js
+++ src/pages/Logon/index.js
@@ -1,9 +1,9 @@
 import React, { useState } from 'react';
-import { Link, useHistory } from 'react-router-dom';
-import { FiLogIn } from 'react-icons/fi';
-import logoImg from '../../assets/logo.svg';
-import heroesImg from '../../assets/heroes.png';
+import { useHistory } from 'react-router-dom';
 import { AuthService } from '../../Services';
+import petImg from '../../img/pets.jpg';
+import logoImg from '../../img/logo.png';
+//import petImgs from '../../../img/pets.jpg';
 import './styles.css';
 
 export default function Logon() {
@@ -25,24 +25,68 @@ export default function Logon() {
     }
 
     return (
-        <div className="logon-container">
-            <section className="form">
-                <img src={logoImg} alt="Be The Hero"/>
-
-                <form onSubmit={handleLogin}>
-                    <h1>Faça seu Login</h1>
-
-                    <input placeholder="Sua ID" value={id} onChange={e => setId(e.target.value)}/>
-                    <button className="button" type="submit">Entrar</button>
-
-                    <Link className="back-link" to="/register">
-                        <FiLogIn size={16} color="#E02041"/>
-                        Não tenho cadastro
-                    </Link>
-                </form>
-            </section>
-
-            <img src={heroesImg} alt="Heroes"/>
+        
+        <section class="form my-4 mx-5">
+        <div class="container">
+            <div class="row no-gutters">
+                <div class="col-lg-6 col-md-6">
+                    <div class="col-lg-12 col-md-10">
+                        <img src={petImg} class="img-fluid" />
+                    </div>
+                    <div class="col-lg-12 col-md-8">
+                        <p class="centertext descriptiontext">
+                            &#128073; Diariamente centenas de animais são abondonados por seus donos. Acesse nossa plataforma e nos ajude a mudar esta realidade,
+                             adotando um amiguinho e sendo o herói.
+                             <br />
+                        </p>
+                        <p>
+                            <ul style={{margin: "30px", fontsize: "large"}}>
+                                <li>Já são mais de &#128054; <b>10.000</b> adoções </li>
+                                <li>Não menos que <b>38.000</b> &#128008; castrações</li>
+                                <li>Entre cães e gatos, são mais de <b>8.000</b> &#128021; cadastros</li>
+                            </ul>
+                        </p>
+                        
+                    </div>
+                </div>
+                <div class="col-lg-6 col-md-6 px-5 pt-5">
+                    <h1 class="font-weight-bold py-3"><img src={logoImg} class="img-fluid" alt="" /></h1>
+                    <h4>Entre com sua conta</h4>
+                    <form>
+                        <div class="form-row">
+                            <div class="col-lg-7">
+                                <input type="email" placeholder="Email" class="form-control my-3 p-4" />
+                            </div>
+                        </div>
+                        <div class="form-row">
+                            <div class="col-lg-7">
+                                <input type="password" placeholder="Senha" class="form-control p-4" />
+                            </div>
+                        </div>
+                        <div class="form-row">
+                            <div class="col-lg-7">
+                                <button type="button" class="btn1 mt-3 mb-1">Logar</button>
+                            </div>
+                        </div>
+                        <div class="form-row">
+                            <div class="col-lg-7">
+                                <button type="button" class="btn2 mb-3"><i class="fab fa-google mr-2"></i>Logar com o Google</button>
+                            </div>
+                        </div>
+                        <p>Ainda não é um herói?<a href="#"> Cadastre-se!</a></p>
+                    </form>
+                </div>
+            </div>
+            <div class="footer">
+                <div class="row">
+                    <div class="col-xs-12 col-sm-12 col-md-12 mt-2 mt-sm-2 text-center text-white">
+                        <p style={{color: "black", textdecoration: "none"}}><a href="#">Seja um Herói</a> 2020
+                        © Todos os direitos Reservados.</p>
+                    </div>
+                    <hr />
+                </div>	
+            </div>
         </div>
+    </section>
     );
 }
