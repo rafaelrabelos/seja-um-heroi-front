5f85a8329a29b8eb2bc80ce4cd914effcd0a3701
Rafael Rabelo <rafael_rabelo@live.com>
2020-10-25 04:40:03 -0300

    roteamento dinamico para layouts

--- src/index.js
+++ src/index.js
@@ -1,11 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
+import { BrowserRouter, Route, Switch, Redirect } from "react-router-dom";
+
+import "@fortawesome/fontawesome-free/css/all.min.css";
 import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/js/bootstrap';
+import 'global.css';
+
+import { LayoutPrincipal, AuthLayout } from 'layout';
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <BrowserRouter>
+    <Switch>
+       <Route path="/auth" render={props => <AuthLayout {...props} />} />
+        <Route path="/hero" render={props => <LayoutPrincipal {...props} />} />
+       <Redirect from="/" to="/hero/index" />
+    </Switch>
+  </BrowserRouter>,
   document.getElementById('root')
 );
---
+++ src/routes.js
@@ -0,0 +1,55 @@
+import { Route, Switch, Redirect} from "react-router-dom";
+import { Logon, Register, Home } from 'pages';
+
+var routes = [
+    {
+      layout: "/hero",
+      page: "/index",
+      nome: "Dashboard",
+      component: Home
+    },
+    {
+      layout: "/auth",
+      page: "/login",
+      nome: "Login",
+      component: Logon
+    },
+    {
+      layout: "/auth",
+      page: "/cadastrar",
+      nome: "Cadastrar",
+      component: Register
+    }
+  ];
+
+  function getPageName(path) {
+    for (let i = 0; i < routes.length; i++) {
+      if (
+        path.indexOf(
+          routes[i].layout + routes[i].path
+        ) !== -1
+      ) {
+        return routes[i].name;
+      }
+    }
+    return "Brand";
+  };
+
+  function getRoutes(routesArray, layout) {
+    return routesArray.map((prop, key) => {
+      if (prop.layout === layout) {
+        return (
+                <Route
+                  path={prop.layout + prop.page}
+                  component={prop.component}
+                  key={key}
+                />
+        );
+      } else {
+        return null;
+      }
+    });
+  };
+
+  export default routes;
+  export {getRoutes, getPageName}
