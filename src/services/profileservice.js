1deb425953881626de967cbd990b11c195d6a299
Rafael A. Rabelos <rafael_rabelo@live.com>
2020-11-26 10:00:18 -0300

    Update profileservice.js
    Atualiza serviço de criação de usuário

--- src/services/profileservice.js
+++ src/services/profileservice.js
@@ -1,7 +1,7 @@
 import api from './api';
 
 export async function Create({nome, email, senha}) {
-    const res = await api.post('/cadastro',{ nome, email, senha });
+    const res = await api.post('/user/create',{ nome, email, senha });
 
     return res;
 }
