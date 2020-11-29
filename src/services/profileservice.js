c1ef8349555b73187f4c81e803a807c45c19ce47
rabelotw <rafael.rabelo@thoughtworks.com>
2020-11-29 03:10:00 -0300

    Ajusta o sistema de cadastro.

--- src/services/profileservice.js
+++ src/services/profileservice.js
@@ -1,7 +1,32 @@
 import api from './api';
 
 export async function Create({nome, email, senha}) {
-    const res = await api.post('/user',{ nome, email, senha });
 
-    return res;
+    const user = process.env.REACT_APP_SYS_BACK_USER;
+    const pass = process.env.REACT_APP_SYS_BACK_PW;
+    let response =  {data: { status: false, erros:[] }}
+
+    if(!user || !pass){
+        response.data.erros = ["Sistema não pode acessar o ambiente externo."]
+        return response;
+    }
+
+    try{
+        const sysLogin = await api.post('/login',{ email: user, senha: pass });
+
+        if(sysLogin.data.status !== true){
+            response.data.erros = ["Sistema não pode se validar corretamente."];
+            return response;
+        }
+
+        const token = sysLogin.data.data.token;
+        const res = await api.post('/user', { nome, email, senha }, { headers: {'Authorization': `Bearer ${token}`} });
+        
+        return res;
+
+    }catch(err){
+        console.log(err.response)
+        response.data.erros = [`${err.response.status}(${err.response.statusText}) Erro na comunicação do sistema.`];
+        return response
+    }
 }
