a643d8f1101776ffd0786520dc244a177efb997b
rabelotw <rafael.rabelo@thoughtworks.com>
2020-12-01 01:09:15 -0300

    Ajuste da home de admin

--- src/pages/admin/home/index.js
+++ src/pages/admin/home/index.js
@@ -5,22 +5,7 @@ export default function AdminDashboard() {
   return (
     <>
       <div className="container form-inline">
-        <div className="card" style={{ width: "18rem", margin: "1%" }}>
-          <img
-            alt="topo"
-            src="/sc_undefined"
-            className="card-img-top"
-            width="30px"
-            height="200px"
-          />
-          <div className="card-body">
-            <h5 className="card-title">Admin</h5>
-            <p className="card-text">Dashboard card</p>
-            <a href="/hero" className="btn btn-primary">
-              Mais Informações
-            </a>
-          </div>
-        </div>
+        <h4>Selecione uma opção para no menu ao lado</h4><br />
       </div>
     </>
   );
